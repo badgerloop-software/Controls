7bbafae410764837505e926e0ed77b355f806046
Nick Beckwith <nbeckwith2@wisc.edu>
2017-04-04 12:51:45 -0500

    Just cleaning stuff up

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include "mbed.h"
 
 DigitalOut myled(LED1);
+DigitalOut led2(LED2);
+
+Serial pc(USBTX, USBRX);
 
 int main() {
   while(1) {
-    myled = 1;
+    led2 = 1;
     wait(1);
-    myled = 0;
+    led2 = 0;
     wait(1);
   }
-
 }
