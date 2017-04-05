ab1c713f6b7feaebd751f2edc7f2b8a4d658f066
Nick Beckwith <nbeckwith2@wisc.edu>
2017-04-05 18:39:45 -0500

    n

--- src/applications/MPU9250AccelTest.hpp
+++ src/applications/MPU9250AccelTest.hpp
@@ -1,7 +1,7 @@
 #ifndef APPLICATIONS_MPU9250ACCELTEST_H
 #define APPLICATIONS_MPU9250ACCELTEST_H
 #include "mbed.h"
-#include "../peripherals/MPU9250.hpp"
 #include "../peripherals/UART.hpp"
+#include "../peripherals/MPU9250.hpp"
 
 #endif
